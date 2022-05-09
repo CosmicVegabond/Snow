d99d3256664595154adb8827205e8374702c5839
Cosmic Nomad <49327511+VcosmicV@users.noreply.github.com>
2022-05-09 04:13:45 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Simple game executable wrapper script example.
-
-# "." is the current working directory, so we put the bundled "./lib" on the
-# library path before the previous contents of that environment variable.
-export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
-
-# Run the game itself.
-./bin/snow
